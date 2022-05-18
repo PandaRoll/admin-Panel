26ad09664c9f6e66ed9ff71ee3bc853e45e40b25
ociuzteam <95280743+ociuzteam@users.noreply.github.com>
2022-05-18 14:13:49 +0530

    drawer

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -1,7 +1,6 @@
+import 'package:admin/screens/main/components/sidebar_package.dart/sidebar_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../dashboard/components/header.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({
@@ -10,55 +9,93 @@ class SideMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            child: ProfileCard(),
-          ),
-          DrawerListTile(
-            title: "Dashboard",
-            svgSrc: "assets/icons/menu_dashbord.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Transaction",
-            svgSrc: "assets/icons/menu_tran.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Task",
-            svgSrc: "assets/icons/menu_task.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Documents",
-            svgSrc: "assets/icons/menu_doc.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Store",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Notification",
-            svgSrc: "assets/icons/menu_notification.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Profile",
-            svgSrc: "assets/icons/menu_profile.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Settings",
-            svgSrc: "assets/icons/menu_setting.svg",
-            press: () {},
-          ),
-        ],
-      ),
+    return Sidebar.fromJson(
+      tabs: [
+        {
+          'title': 'Chapter A',
+          'children': [
+            {'title': 'Chapter A1'},
+            {'title': 'Chapter A2'},
+            {'title': 'Chapter A3'},
+            {
+              'title': 'Chapter A4',
+              'children': [
+                {'title': 'Chapter A41'},
+                {'title': 'Chapter A42'},
+                {'title': 'Chapter A43'},
+              ],
+            },
+          ],
+        },
+        {
+          'title': 'Chapter B',
+          'children': [
+            {'title': 'Chapter B1'},
+            {
+              'title': 'Chapter B2',
+              'children': [
+                {'title': 'Chapter B2a'},
+                {'title': 'Chapter B2b'},
+              ],
+            },
+          ],
+        },
+        {'title': 'Chapter C'},
+      ],
+      onTabChanged: (string) {
+        print(string);
+      },
     );
+
+    //  Drawer(
+    //   child: ListView(
+    //     children: [
+    //       DrawerHeader(
+    //         child: ProfileCard(),
+    //       ),
+    //       DrawerListTile(
+    //         title: "Dashboard",
+    //         svgSrc: "assets/icons/menu_dashbord.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Transaction",
+    //         svgSrc: "assets/icons/menu_tran.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Task",
+    //         svgSrc: "assets/icons/menu_task.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Documents",
+    //         svgSrc: "assets/icons/menu_doc.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Store",
+    //         svgSrc: "assets/icons/menu_store.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Notification",
+    //         svgSrc: "assets/icons/menu_notification.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Profile",
+    //         svgSrc: "assets/icons/menu_profile.svg",
+    //         press: () {},
+    //       ),
+    //       DrawerListTile(
+    //         title: "Settings",
+    //         svgSrc: "assets/icons/menu_setting.svg",
+    //         press: () {},
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
 
